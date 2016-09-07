91a6401b9a5d61b99108768a2a2b1d93499a34d7
Juan José Mata <juanjo.mata@gmail.com>
2016-09-07 08:32:08 -0700

    Fix ORM definition

--- db/migrate/20160907083000_create_devices.rb
+++ db/migrate/20160907083000_create_devices.rb
@@ -1,9 +1,9 @@
 Sequel.migration do
   change do
     create_table :devices do
-      foreign_key :user_id, :users, type: :uuid, null: false, on_delete: :cascade, primary_key: true
+      foreign_key :user_id, :users, type: 'uuid', null: false, on_delete: :cascade, primary_key: true
 
-      Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+      Uuid :id, default: 'uuid_generate_v4()'.lit
 
       String :devices, null: false, type: 'json', default: '{}'
 
@@ -13,14 +13,12 @@ Sequel.migration do
 
     alter_table :devices do
       add_index :user_id
-      add_index :users
     end
   end
 
   down do
     alter_table :devices do
       drop_index :user_id
-      drop_index :users
     end
 
     drop_table :devices
