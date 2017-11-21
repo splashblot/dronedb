b580973e932123ed7491392674985f99d64e7b3e
Adrián Pérez <aloha@adrianperez.info>
2017-11-21 11:43:36 +0000

    raster styles type for geom class

--- lib/carto/styles/geometry.rb
+++ lib/carto/styles/geometry.rb
@@ -19,7 +19,8 @@ module Carto::Styles
     NAMES_CLASSES_MAP = {
       "layer['mapnik::geometry_type'=1]" => Carto::Styles::Point,
       "layer['mapnik::geometry_type'=2]" => Carto::Styles::Line,
-      "layer['mapnik::geometry_type'=3]" => Carto::Styles::Polygon
+      "layer['mapnik::geometry_type'=3]" => Carto::Styles::Polygon,
+      "layer" => Carto::Styles::Raster,
     }.freeze
 
     def to_cartocss
---
+++ lib/carto/styles/raster.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+require_relative './style.rb'
+require_relative '../definition.rb'
+
+module Carto::Styles
+  class Raster < Style
+    def initialize(definition: default_definition)
+      super(definition)
+    end
+
+    def self.accepted_geometry_types
+      ['raster', 'the_raster_webmercator']
+    end
+
+    def default_definition
+      Carto::Definition.instance.load_from_file(CARTOGRAPHY_DEFINITION_LOCATION)[:simple][:point]
+    end
+
+    private
+
+    def parse_opacity(opacity)
+      ["raster-opacity: #{opacity};"]
+    end
+
+  end
+end
