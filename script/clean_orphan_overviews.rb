de617fbd6bd2aa0ad4967d5bd7ef89b597623ae7
Adrián <aloha@adrianperez.info>
2018-01-26 13:13:45 +0000

    clean orphan raster overviews for all users

--- script/clean_orphan_overviews.rb
+++ script/clean_orphan_overviews.rb
@@ -1,21 +1,30 @@
 require 'byebug'
 
-def remove_overviews raster
+def remove_overviews(raster,user,schema)
   i = 0
   while i < 32
     exp = 2**i
-    User.where(username: "dev").first().in_database["DROP TABLE IF EXISTS o_#{exp}_#{raster} CASCADE"].first()
+    puts "the table o_#{exp}_#{raster} from dev"
+    # User.where(username: "#{user}").first().in_database["DROP TABLE IF EXISTS #{schema}.o_#{exp}_#{raster} CASCADE"].first()
     i += 1
   end
 end
 
-orphans = User.where(username: "dev").first().in_database["SELECT table_name FROM information_schema.tables WHERE  table_name LIKE 'o\_1\_%\_raster%' and table_name NOT LIKE 'o\_16\_%\_raster%'"].all()
-orphans.each do |ov|
-  #search for canonical table still in use
-  ov[:table_name].slice! "o_1_"
-  begin
-    User.where(username: "dev").first().in_database["SELECT cartodb_id FROM public.#{ov[:table_name]} LIMIT 1"].first()
-  rescue
-    remove_overviews ov[:table_name]
+def check_orphans(user,schema)
+  orphans = User.where(username: "#{user}").first().in_database["SELECT table_name FROM information_schema.tables WHERE  table_name LIKE 'o\_1\_%\_raster%' and table_name NOT LIKE 'o\_16\_%\_raster%'"].all()
+  orphans.each do |ov|
+    #search for canonical table still in use
+    ov[:table_name].slice! "o_1_"
+    begin
+      User.where(username: "#{user}").first().in_database["SELECT cartodb_id FROM #{schema}.#{ov[:table_name]} LIMIT 1"].first()
+    rescue
+      remove_overviews(ov[:table_name],user,schema)
+    end
   end
+end
+
+User.all do |u|
+  puts "########################## RASTER OVERVIEW USER"
+  puts "Checking orphan overviews for: " +u.username.to_s
+  check_orphans(u.username,u.database_schema)
 end
